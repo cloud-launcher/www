d88b889bf6ff8fb39bd1e17f894dc3b30a4d7f31
Blake La Pierre <blakelapierre@gmail.com>
2015-02-17 06:22:25 -0800

    Still needs work.

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -4,6 +4,7 @@ var angular = require('angular');
 module.exports = {
   'base-angular': angular.module('base-angular', [])
     .directive('center', require('./modules/base-angular/center/directive'))
-    .directive('helloWorld', require('./modules/base-angular/helloWorld/directive'))
-    .directive('spinner',    require('./modules/base-angular/spinner/directive'))
+  ,
+  'cloud-launcher': angular.module('cloud-launcher', ['base-angular'])
+    .directive('teaser', require('./modules/cloud-launcher/teaser/directive'))
 };

5a701f893dfe32afc18bcbe3b860bc945ce971f0
Blake La Pierre <blakelapierre@gmail.com>
2015-02-20 01:19:55 -0800

    This looks pretty good. Will likely build for Alpha Marketing Preview.

--- frontend/app/modules/cloud-launcher/dockerSearch/directive.js
+++ frontend/app/modules/cloud-launcher/dockerSearch/directive.js
@@ -1,35 +1,45 @@
 const _ = require('lodash');
 
 module.exports = ['$resource', $resource => {
-  const registry = $resource('https://registry.hub.docker.com/v1/search?q=:query&n=10');
+  const registry = $resource('https://registry.hub.docker.com/v1/search?q=:query&n=:count&page=:page');
   return {
     restrict: 'E',
     template: require('./template.html'),
     link: ($scope, element, attributes) => {
-      const input = element.find('input');
+      const input = element.find('input'),
+            docker = {};
+
+      $scope.docker = docker;
 
       input.on('keyup', $event => {
         const value = input.val();
         debouncedSearch(value);
 
         $scope.$apply(() => {
-          $scope.docker.querying = true;
+          docker.querying = true;
+          docker.showResults = value !== '';
         });
       });
 
       // Going to have issues with requests returning out-of-order...
       const debouncedSearch = _.debounce(query => {
-        const result = registry.get({query} , () => {
-          console.log(result);
+        const result = registry.get({query, count: 100, page: 1} , () => {
+          if (result.query != query) {
+            console.log('didn\'t match', query, result.query);
+            return;
+          }
 
-          $scope.docker.querying = false;
-          $scope.docker.results = _.sortBy(result.results, 'star_count').reverse();
+          const {num_pages, num_results, page, page_size, results} = result;
 
-          $scope.docker.showResults = query !== '';
+          docker.querying = false;
+          docker.results = _.sortBy(results, 'star_count').reverse();
+          docker.start = page * page_size - page_size + 1;
+          docker.end = Math.min(docker.start + page_size - 1, num_results);
+          docker.total = num_results;
+
+          docker.showResults = query !== '';
         });
       }, 500);
-
-      $scope.docker = {};
     },
     controller: ['$scope', $scope => {
 

487b20874eb6d3e7c1b06e10e4c0253fbf71499a
Blake La Pierre <blakelapierre@gmail.com>
2015-02-20 17:15:17 -0800

    Some visual improvements.

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -3,7 +3,7 @@ var angular = require('angular');
 
 module.exports = {
   'cloud-launcher': angular.module('cloud-launcher', ['ngResource'])
-    .directive('dockerSearch', require('./modules/cloud-launcher/dockerSearch/directive'))
     .directive('launcherUi', require('./modules/cloud-launcher/launcherUi/directive'))
+      .directive('dockerSearch', require('./modules/cloud-launcher/launcherUi/dockerSearch/directive'))
     // .directive('teaser', require('./modules/cloud-launcher/teaser/directive'))
 };

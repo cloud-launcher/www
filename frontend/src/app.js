7e19139d5ef09bbd4dfe312b1db4a3e3238a16d0
Blake La Pierre <blakelapierre@gmail.com>
2015-02-22 19:05:09 -0800

    Break up into multiple directives and some cost styling fixes.

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,7 +3,12 @@ require('angular-resource');
 
 module.exports = {
   'cloud-launcher': angular.module('cloud-launcher', ['ngResource'])
-    .directive('launcherUi', require('./modules/cloud-launcher/launcherUi/directive'))
-      .directive('dockerSearch', require('./modules/cloud-launcher/launcherUi/dockerSearch/directive'))
+    .directive('launcherUi',      require('./modules/cloud-launcher/launcherUi/directive'))
+
+      .directive('cost',          require('./modules/cloud-launcher/launcherUi/cost/directive'))
+      .directive('description',   require('./modules/cloud-launcher/launcherUi/description/directive'))
+      .directive('dockerSearch',  require('./modules/cloud-launcher/launcherUi/dockerSearch/directive'))
+      .directive('launch',        require('./modules/cloud-launcher/launcherUi/launch/directive'))
+      .directive('providers',     require('./modules/cloud-launcher/launcherUi/providers/directive'))
     // .directive('teaser', require('./modules/cloud-launcher/teaser/directive'))
 };

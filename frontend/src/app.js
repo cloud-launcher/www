b0eb30a6bbaac2c845d4a2e56f4bec83a2f58fa8
Blake La Pierre <blakelapierre@gmail.com>
2015-02-23 17:58:44 -0800

    Move target to match DOM structure

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -6,8 +6,8 @@ module.exports = {
     .directive('launcherUi',      require('./modules/cloud-launcher/launcherUi/directive'))
 
       .directive('description',       require('./modules/cloud-launcher/launcherUi/description/directive'))
+        .directive('target',          require('./modules/cloud-launcher/launcherUi/description/target/directive'))
         .directive('configuration',   require('./modules/cloud-launcher/launcherUi/description/configuration/directive'))
-          .directive('target',        require('./modules/cloud-launcher/launcherUi/description/configuration/target/directive'))
         .directive('cost',            require('./modules/cloud-launcher/launcherUi/description/cost/directive'))
       .directive('dockerSearch',      require('./modules/cloud-launcher/launcherUi/dockerSearch/directive'))
       .directive('launch',            require('./modules/cloud-launcher/launcherUi/launch/directive'))

15a504f613b88691a7ca1c4020e14259155d8ee6
Blake La Pierre <blakelapierre@gmail.com>
2015-03-01 16:10:41 -0800

    Some code for loading all results (currently disabled).

--- frontend/src/modules/cloud-launcher/launcherUi/dockerSearch/directive.js
+++ frontend/src/modules/cloud-launcher/launcherUi/dockerSearch/directive.js
@@ -14,6 +14,9 @@ module.exports = ['$resource', $resource => {
 
       $scope.queryChanged = $event => {
         const value = docker.query;
+
+        docker.haveFirstResults = false;
+
         if (value !== '') {
           debouncedSearch(value);
           docker.querying = true;
@@ -30,7 +33,7 @@ module.exports = ['$resource', $resource => {
 
       // Going to have issues with requests returning out-of-order...
       const debouncedSearch = _.debounce(query => {
-        const result = registry.get({query, count: 100, page: 1} , () => {
+        const result = registry.get({query, count: 1000, page: 1} , () => {
           if (result.query != query) {
             console.log('didn\'t match', query, result.query);
             return;
@@ -38,15 +41,56 @@ module.exports = ['$resource', $resource => {
 
           const {num_pages, num_results, page, page_size, results} = result;
 
+          docker.haveFirstResults = true;
           docker.querying = false;
           docker.results = _.sortBy(results, 'star_count').reverse();
+          docker.page = 1;
           docker.start = page * page_size - page_size + 1;
           docker.end = Math.min(docker.start + page_size - 1, num_results);
+          docker.end = docker.results.length;
           docker.total = num_results;
 
           docker.showResults = query !== '';
+
+          docker.resultsLoaded = docker.results.length / num_results * 100;
+
+          // if (num_results > docker.results.length) loadRestOfResults(docker);
         });
       }, 500);
+
+      function loadRestOfResults(docker) {
+        let {query, page} = docker;
+        docker.querying = true;
+
+        page += 1;
+
+        const result = registry.get({query, count: 100, page}, () => {
+          if (result.query != query) {
+            console.log('didn\'t match', query, result.query);
+            return;
+          }
+
+          const {num_pages, num_results, page_size, results} = result;
+
+          docker.page = page;
+
+          docker.results.splice(0, 0, ...results);
+          docker.results = _.sortBy(docker.results, 'star_count').reverse();
+          //docker.results = _.sortBy(docker.results.concat(results), 'star_count').reverse();
+
+          docker.end = docker.results.length;
+          docker.total = num_results;
+
+          docker.resultsLoaded = docker.results.length / num_results * 100;
+
+          if (num_results > docker.results.length) {
+            loadRestOfResults(docker);
+          }
+          else {
+            docker.querying = false;
+          }
+        });
+      }
     },
     controller: ['$scope', $scope => {
 

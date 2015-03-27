aebe007466a693f74c57fe03f245b8d2c7dabfb7
Blake La Pierre <blakelapierre@gmail.com>
2015-03-27 03:20:34 -0700

    Update dependencies. Include simulator.

--- frontend/src/modules/cloud-launcher/services/launchCloud/service.js
+++ frontend/src/modules/cloud-launcher/services/launchCloud/service.js
@@ -1,8 +1,11 @@
-var launchCloudBrowser = require('launch-cloud-browser');
+var launchCloudBrowser = require('launch-cloud-browser'),
+    launchCloudSimulator = require('launch-cloud-browser-simulator');
 
 module.exports = () => {
+  console.log(window.location.search);
   const providerConfigs = {},
-        launchCloud = launchCloudBrowser(providerConfigs, (...args) => console.log(...args));
-
+        launchCloud = window.location.search === '?simulator' ? launchCloudSimulator(providerConfigs, (...args) => console.log(...args))
+                                                             : launchCloudBrowser(providerConfigs, (...args) => console.log(...args));
+console.log(launchCloud);
   return launchCloud;
 };

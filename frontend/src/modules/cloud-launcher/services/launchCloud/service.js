014f53ec79a831307d0a29e842bf2d393e96bd3a
Blake La Pierre <blakelapierre@gmail.com>
2015-03-29 04:48:49 -0700

    General improvements.

--- frontend/src/modules/cloud-launcher/services/launchCloud/service.js
+++ frontend/src/modules/cloud-launcher/services/launchCloud/service.js
@@ -1,9 +1,11 @@
 var launchCloudBrowser = require('launch-cloud-browser'),
     launchCloudSimulator = require('launch-cloud-browser-simulator');
 
-module.exports = () => {
+module.exports = ['$location', $location => {
+  console.log($location, $location.search());
+
   const providerConfigs = {},
         launchCloud = window.location.search === '?simulator' ? launchCloudSimulator(providerConfigs, (...args) => console.log(...args))
                                                              : launchCloudBrowser(providerConfigs, (...args) => console.log(...args));
   return launchCloud;
-};
+}];

58d43074fd0b54280017d5af00c0ffa88ba7f378
Blake La Pierre <blakelapierre@gmail.com>
2015-03-31 14:01:06 -0700

    Indent change. Print some log messages to console.

--- frontend/src/modules/cloud-launcher/directives/clouds/directive.js
+++ frontend/src/modules/cloud-launcher/directives/clouds/directive.js
@@ -5,132 +5,137 @@ module.exports = () => {
     restrict: 'E',
     template: require('./template.html'),
     controller: [
-    '$scope', 'launchCloud', 'providerMonitor', 'storedClouds',
-    ($scope, launchCloud, providerMonitor, storedClouds) => {
-      const {providers} = launchCloud,
-            clouds = storedClouds.getClouds();
+      '$scope', 'launchCloud', 'providerMonitor', 'storedClouds',
+      ($scope, launchCloud, providerMonitor, storedClouds) => {
+        const {providers} = launchCloud,
+              clouds = storedClouds.getClouds();
 
-      $scope.destroyLog = {};
-      $scope.destroyStatus = {};
-      $scope.hideClusters = {};
-      $scope.hideMachines = {};
-      $scope.clouds = clouds;
+        $scope.destroyLog = {};
+        $scope.destroyStatus = {};
+        $scope.hideClusters = {};
+        $scope.hideMachines = {};
+        $scope.clouds = clouds;
 
-      $scope.$watchCollection('clouds', clouds => {
-        const providers = _.unique(
-                            _.flatten(
-                              _.map(
-                                clouds,
-                                cloud => _.map(cloud.clusters, cluster => { return cluster.provider; }))));
+        $scope.$watchCollection('clouds', clouds => {
+          const providers = _.unique(
+                              _.flatten(
+                                _.map(
+                                  clouds,
+                                  cloud => _.map(cloud.clusters, cluster => { return cluster.provider; }))));
 
-        _.each(providers, provider => providerMonitor.monitor(provider, 15000, updateMachines));
-      });
-
-      let currentHoveredCloud;
-      $scope.cloudHovered = cloud => {
-        if (currentHoveredCloud != cloud) {
-          currentHoveredCloud = cloud;
-
-          $scope.showCloudPoints(cloud);
-        }
-      };
-
-      $scope.cloudNotHovered = cloud => {
-        if (currentHoveredCloud === cloud) {
-          currentHoveredCloud = undefined;
-        }
-      };
-
-      $scope.destroyCluster = cluster => {
-        const {id} = cluster;
-        console.log('destroying cluster', id);
-
-        // launchCloud.destroy()
-      };
-
-      $scope.destroyCloud = cloud => {
-        const {id} = cloud;
-
-        $scope.hideClusters[cloud.id] = false;
-        $scope.destroyLog[cloud.id] = [];
-        $scope.destroyStatus[cloud.id] = 'Destroying';
-
-        launchCloud
-          .destroy(cloud, destroyLog(cloud, providers))
-          .then(
-            cloud => {
-              console.log('destroyed', cloud);
-              storedClouds.removeCloud(cloud);
-
-              if ($scope.clouds.length === 0) $scope.gotoStage('launchpad');
-
-              delete $scope.destroyLog[cloud.id];
-
-              $scope.$apply();
-            },
-            error => {
-              console.log('error destroying', error);
-            }
-          );
-      };
-
-      $scope.forgetCloud = cloud => {
-        storedClouds.removeCloud(cloud);
-
-        if ($scope.clouds.length === 0) $scope.gotoStage('launchpad');
-      };
-
-      $scope.returnToLaunchStatus = () => {
-        $scope.gotoStage('launchstatus');
-      };
-
-      $scope.getMachineProviderDashboardUrl = machine => {
-        return `http://cloud.digitalocean.com/droplets/${machine.providerData.id}`;
-      };
-
-      $scope.getMachineEstimatedCost = (cluster, machine) => {
-        const now = new Date(),
-              {createdAt, size} = machine,
-              {provider} = cluster,
-              {profile} = providers[provider],
-              currentLife = Math.ceil((now - new Date(createdAt)) / 1000 / 60 / 60),
-              estimatedCost = currentLife * profile.sizes[size].price_hourly;
-
-        return estimatedCost;
-      };
-
-      function updateMachines(providerName, machines) {
-        let gotNew = false;
-
-        _.each(clouds, cloud => {
-          _.each(cloud.clusters, cluster => {
-            _.each(cluster.machines, (machine, id) => {
-              const machineUpdate = machines[id];
-
-              if (!_.matches(machineUpdate)(machine.providerData)) {
-                gotNew = true;
-                machine.providerData = machineUpdate;
-
-                _.merge(machine, machineUpdate);
-              }
-            });
-          });
+          _.each(providers, provider => providerMonitor.monitor(provider, 15000, updateMachines));
         });
 
-        if (gotNew) {
-          storedClouds.saveClouds();
-          $scope.$apply();
-        }
-        else {
-          const monitor = providerMonitor.cancel(providerName);
-        }
-      }
+        let currentHoveredCloud;
+        $scope.cloudHovered = cloud => {
+          if (currentHoveredCloud != cloud) {
+            currentHoveredCloud = cloud;
 
-      function destroyLog(cloud, providers) {
-        return event => {
-          $scope.destroyLog[cloud.id].push(event);
+            $scope.showCloudPoints(cloud);
+          }
         };
+
+        $scope.cloudNotHovered = cloud => {
+          if (currentHoveredCloud === cloud) {
+            currentHoveredCloud = undefined;
+          }
+        };
+
+        $scope.destroyCluster = cluster => {
+          const {id} = cluster;
+          console.log('destroying cluster', id);
+
+          // launchCloud.destroy()
+        };
+
+        $scope.destroyCloud = cloud => {
+          const {id} = cloud;
+
+          $scope.hideClusters[cloud.id] = false;
+          $scope.destroyLog[cloud.id] = [];
+          $scope.destroyStatus[cloud.id] = 'Destroying';
+
+          launchCloud
+            .destroy(cloud, destroyLog(cloud, providers))
+            .then(
+              cloud => {
+                console.log('destroyed', cloud);
+                storedClouds.removeCloud(cloud);
+
+                if ($scope.clouds.length === 0) $scope.gotoStage('launchpad');
+
+                delete $scope.destroyLog[cloud.id];
+
+                $scope.$apply();
+              },
+              error => {
+                console.log('error destroying', error);
+              }
+            );
+        };
+
+        $scope.forgetCloud = cloud => {
+          storedClouds.removeCloud(cloud);
+
+          if ($scope.clouds.length === 0) $scope.gotoStage('launchpad');
+        };
+
+        $scope.returnToLaunchStatus = () => {
+          $scope.gotoStage('launchstatus');
+        };
+
+        $scope.getMachineProviderDashboardUrl = machine => {
+          return `http://cloud.digitalocean.com/droplets/${machine.providerData.id}`;
+        };
+
+        $scope.getMachineEstimatedCost = (cluster, machine) => {
+          const now = new Date(),
+                {createdAt, size} = machine,
+                {provider} = cluster,
+                {profile} = providers[provider],
+                currentLife = Math.ceil((now - new Date(createdAt)) / 1000 / 60 / 60),
+                estimatedCost = currentLife * profile.sizes[size].price_hourly;
+
+          return estimatedCost;
+        };
+
+        function updateMachines(providerName, machines) {
+          let gotNew = false;
+
+          _.each(clouds, cloud => {
+            _.each(cloud.clusters, cluster => {
+              _.each(cluster.machines, (machine, id) => {
+                const machineUpdate = machines[id];
+
+                if (!_.matches(machineUpdate)(machine.providerData)) {
+                  gotNew = true;
+                  machine.providerData = machineUpdate;
+
+                  _.merge(machine, machineUpdate);
+                }
+              });
+            });
+          });
+
+          if (gotNew) {
+            storedClouds.saveClouds();
+            $scope.$apply();
+          }
+          else {
+            const monitor = providerMonitor.cancel(providerName);
+          }
+        }
+
+        function destroyLog(cloud, providers) {
+          return (...args) => {
+            if (args && args.length === 1) {
+              const [event] = args;
+              $scope.destroyLog[cloud.id].push(event);
+            }
+            else console.log(...args);
+          };
+        }
       }
-    }]
+    ]
   };
 };

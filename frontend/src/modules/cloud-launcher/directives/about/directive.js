b4c6f735c8997b0471d5db02c09ee9d26e2814c8
Blake La Pierre <blakelapierre@gmail.com>
2015-04-10 15:55:07 -0700

    Update about

--- frontend/src/modules/cloud-launcher/directives/about/directive.js
+++ frontend/src/modules/cloud-launcher/directives/about/directive.js
@@ -1,6 +1,13 @@
 module.exports = () => {
   return {
     restrict: 'E',
-    template: require('./template.html')
+    template: require('./template.html'),
+    scope: {},
+    controller: ['$scope', $scope => {
+      $scope.toggle = panel => {
+        if ($scope.selectedCopy === panel) $scope.selectedCopy = undefined;
+        else $scope.selectedCopy = panel;
+      };
+    }]
   };
 };
